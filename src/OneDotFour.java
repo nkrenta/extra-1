5d60f597afbd465a90b04291ffc8bd1077f34799
origin <nkrenta@inbox.ru>
2024-09-01 21:06:56 +0300

    Init

---
+++ src/OneDotFour.java
@@ -0,0 +1,5 @@
+public class OneDotFour {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/OneDotSeven.java
@@ -0,0 +1,5 @@
+public class OneDotSeven {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
